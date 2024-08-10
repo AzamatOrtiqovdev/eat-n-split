dcbdbbbb73b633d1061aed3ea15851bd11cb9d35
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-08-10 12:55:22 +0500

    creating form elements

--- src/App.js
+++ src/App.js
@@ -23,8 +23,12 @@ export default function App() {
   return (
     <div className="app">
       <div className="sidebar">
-      <FriendsList />
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -58,7 +62,50 @@ function Friend({ friend }) {
         </p>
       )}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend Name</label>
+      <input type="text" />
+
+      <label>🌉 Image Url</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍 Your Expense</label>
+      <input type="text" />
+
+      <label>👫 X's Expense</label>
+      <input type="text" disabled/>
+
+      <label>🤑 Who is paying the bill ?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  )
 }
